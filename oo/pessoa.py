47cc2f2fa5203cc628b241770a3a1c6f2ae379d4
benzersiz <bueno646@gmail.com>
2021-03-25 16:04:07 -0300

    Criado o atributo de instância nome, idade e filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,16 +1,18 @@
 class Pessoa:
-    def __init__(self, nome = None, idade=40):
+    def __init__(self, *filhos, nome = None, idade=40):
         self.nome = nome
         self.idade = idade
+        self.filhos= list(filhos)
 
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa('Luciano')
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome='Mariana'
-    print(p.nome)
+    pedro = Pessoa(nome='Pedro')
+    luciano = Pessoa(pedro, nome='Luciano')
+    print(Pessoa.cumprimentar(luciano))
+    print(id(luciano))
+    print(luciano.cumprimentar())
+    print(luciano.nome)
+    for filho in luciano.filhos:
+        print(filho.nome)
